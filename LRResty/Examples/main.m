5e48bdc725fe12f56e4d2e02093d607eadcf137f
Calvin Buckley <calvin@cmpct.info>
2022-03-08 23:27:48 -0400

    Purge LRResty

--- LRResty/Examples/main.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-int main(int argc, char *argv[]) {
-  
-  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-  int retVal = UIApplicationMain(argc, argv, nil, @"ExampleAppDelegate");
-  [pool release];
-  return retVal;
-}
--- LRResty/Scripts/ServerControl.rb
+++
@@ -1,5 +0,0 @@
-if ARGV[0] == 'start'
-  `thin -t1 -d -p 10090 -R Tests/ServiceStub/config.ru start `
-else
-  `thin stop`
-end
