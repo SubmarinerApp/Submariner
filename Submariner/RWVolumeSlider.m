4a5e125e428fc9b00c2f655e2d07ab44b23d88c5
Calvin Buckley <calvin@cmpct.info>
2022-03-19 13:51:28 -0300

    Nop out custom volume slider
    Looks bad on modern macOS. Cna be fully purged once the nibs are
    upgraded.

--- Submariner/RWVolumeSlider.m
+++ Submariner/RWVolumeSlider.m
@@ -37,23 +37,5 @@
 
 @implementation RWVolumeSlider
 
-+ (Class)cellClass {
-    return [RWVolumeSliderCell class];
-}
-
-
-- (void)awakeFromNib {
-    RWVolumeSliderCell *newCell = [[RWVolumeSliderCell alloc] init];
-    id oldCell = [self cell];
-    [newCell setImage:[oldCell image]];
-    [newCell setMinValue:[oldCell minValue]];
-    [newCell setMaxValue:[oldCell maxValue]];
-    [newCell setSliderType:[oldCell sliderType]];
-    [newCell setAction:[oldCell action]];
-    [newCell setTarget:[oldCell target]];
-    
-    [self setCell:newCell];
-    [newCell release];
-}
 
 @end
