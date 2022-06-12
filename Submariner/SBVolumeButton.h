85d3090fb34439882ecdf54b841d5c01efcb2ff3
Calvin Buckley <calvin@cmpct.info>
2022-06-12 18:52:57 -0300

    Volume button with scroll wheel support

---
+++ Submariner/SBVolumeButton.h
@@ -0,0 +1,17 @@
+//
+//  SBVolumeButton.h
+//  Submariner
+//
+//  Created by Calvin Buckley on 2022-06-12.
+//  Copyright © 2022 Calvin Buckley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBVolumeButton : NSButton
+
+@end
+
+NS_ASSUME_NONNULL_END
