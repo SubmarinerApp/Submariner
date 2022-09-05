2a6cf0387512bc82411018c23729830c615710ee
Calvin Buckley <calvin@cmpct.info>
2022-09-05 16:44:00 -0300

    same for local

--- Submariner/SBMusicController.m
+++ Submariner/SBMusicController.m
@@ -381,7 +381,7 @@
 - (IBAction)addTrackToTracklist:(id)sender {
 
     NSIndexSet *indexSet = [tracksTableView selectedRowIndexes];
-    __weak NSMutableArray *tracks = [NSMutableArray array];
+    NSMutableArray *tracks = [NSMutableArray array];
     
     [indexSet enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
         [tracks addObject:[[tracksController arrangedObjects] objectAtIndex:idx]];
