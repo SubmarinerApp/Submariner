edcc726907d1b0a798f8bbbaf7182fcdfefd5224
Calvin Buckley <calvin@cmpct.info>
2022-09-13 18:10:17 -0300

    Add track artist name transformer
    Handles the legacy case (of just me really) of nil track artists.
    Could be more flexible in the future.

---
+++ Submariner/SBTrackArtistNameTransformer.h
@@ -0,0 +1,17 @@
+//
+//  SBTrackArtistNameTransformer.h
+//  Submariner
+//
+//  Created by Calvin Buckley on 2022-09-13.
+//  Copyright © 2022 Calvin Buckley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBTrackArtistNameTransformer : NSValueTransformer
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Submariner/SBTrackArtistNameTransformer.m
@@ -0,0 +1,34 @@
+//
+//  SBTrackArtistNameTransformer.m
+//  Submariner
+//
+//  Created by Calvin Buckley on 2022-09-13.
+//  Copyright © 2022 Calvin Buckley. All rights reserved.
+//
+
+#import "SBTrackArtistNameTransformer.h"
+#import "SBTrack.h"
+#import "SBAlbum.h"
+#import "SBArtist.h"
+
+@implementation SBTrackArtistNameTransformer
+
+
++ (Class)transformedValueClass {
+    return [NSString class];
+}
+
++ (BOOL)allowsReverseTransformation {
+    return NO;
+}
+
+- (id)transformedValue:(id)value {
+    SBTrack *track = (SBTrack*)value;
+    // return album artist if we don't have an artist
+    if (track.artistName == nil || [track.artistName isEqualToString: @""]) {
+        return track.album.artist.itemName;
+    }
+    return track.artistName;
+}
+
+@end
