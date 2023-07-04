0bd239785d07e4179f77693b0eff6e7605065afc
Calvin Buckley <calvin@cmpct.info>
2023-07-04 12:04:01 -0300

    we don't need this because we always pass auth as query string params

--- Submariner/SBClientController.swift
+++ Submariner/SBClientController.swift
@@ -29,18 +29,7 @@ fileprivate let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, catego
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         let request = URLRequest(url: url)
-        
-        if let username = server.username, let password = server.password {
-            let loginString = "\(username):\(password)"
-            if let loginData = loginString.data(using: .utf8) {
-                let base64login = loginData.base64EncodedString()
-                let authHeader = "Basic \(base64login)"
-                config.httpAdditionalHeaders = ["Authorization": authHeader]
-            }
-        } else {
-            logger.warning("Empty username/password, no authorization header will be included.")
-            logger.warning("username nil? \(self.server.username == nil) password nil? \(self.server.password == nil)")
-        }
+        // No auth header needed since we just pass them over query string
         
         let task = session.dataTask(with: request) { data, response, error in
             // sensitive because &p= contains user password
