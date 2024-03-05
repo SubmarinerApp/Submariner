12b285b009fc3e555bb9c3b0da7adf0370218e39
Calvin Buckley <calvin@cmpct.info>
2024-03-05 03:41:24 -0400

    clean up imagebrowserview cruft

--- Submariner/SBAlbum.swift
+++ Submariner/SBAlbum.swift
@@ -12,46 +12,13 @@ import CoreData
 
 @objc(SBAlbum)
 public class SBAlbum: SBMusicItem {
-    // #MARK: - IKImageBrowserView
-    
-    // HACK: because IKImageBrowserView gets the smallest size by default. unneeded in the future?
-    static let nullCover = {
-        // 600 -> max size in image browser view for now
-        let config = NSImage.SymbolConfiguration(pointSize: 600, weight: .regular)
-        let image = NSImage(systemSymbolName: "questionmark.square.dashed", accessibilityDescription: "No Album Art")
-        return image!.withSymbolConfiguration(config)
-    }()
-    
-    override public func imageTitle() -> String? {
-        return itemName
-    }
-    
-    override public func imageUID() -> String? {
-        return itemName
-    }
-    
-    override public func imageRepresentationType() -> String! {
-        // XXX: Can't use IKImageBrowserPathRepresentationType because [SBTrack coverImage] calls imageRepresentation.
-        return IKImageBrowserNSImageRepresentationType
-    }
+    static let nullCover = NSImage(systemSymbolName: "questionmark.square.dashed", accessibilityDescription: "No Album Art")
     
     override public func imageRepresentation() -> Any! {
         if let cover = self.cover, let path = cover.imagePath as String? {
             return NSImage.init(byReferencingFile: path)
         }
         return SBAlbum.nullCover;
-    }
-    
-    override public func imageVersion() -> Int {
-        // Avoid constructing an image. I think this is only really used to check if it's loaded,
-        // since the album artwork shouldn't change normally (and if it does, rare it'll be the same size).
-        // XXX: Better method.
-        if let cover = self.cover,
-           let path = cover.imagePath as String?,
-           let attribs = try? FileManager.default.attributesOfItem(atPath: path) {
-            return attribs[FileAttributeKey.size] as! Int
-        }
-        return 0
     }
     
     @objc var starredBool: Bool {
